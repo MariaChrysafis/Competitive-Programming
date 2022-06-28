d540bd2de2b3295a34be09560be14541bac4a891
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2022-06-28 15:45:04 -0700

    Create 1698A.cpp

---
+++ Codeforces/1698A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+void solve () {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << arr[0] << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
